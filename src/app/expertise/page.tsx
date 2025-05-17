13a45245a927f871ea870f024a94a0313b7af22f
igorchugurov <igorchugurov@gmail.com>
2025-05-17 20:40:50 +0200

    05172025

--- src/app/expertise/page.tsx
+++ src/app/expertise/page.tsx
@@ -7,7 +7,9 @@ export default function Page() {
     <main className="flex flex-1 flex-col  bg-white overflow-auto">
       <Header />
       <div className="flex flex-col flex-1  overflow-hidden">
-        <div className="flex-1 flex flex-col overflow-y-auto  scrollbar-hide"></div>
+        <div className="flex-1 flex flex-col overflow-y-auto  scrollbar-hide">
+          <h1>Coming soon</h1>
+        </div>
         <Footer />
       </div>
     </main>
